5ba3e5056d57467ae374705a4affaa76a7459a5e
brian-nyabuto12 <brian.nyabuto@student.moringaschool.com>
2021-05-22 07:13:11 +0300

    add components and classes for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RepoComponent } from './repo/repo.component';
+import { SearchComponent } from './search/search.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RepoComponent,
+    SearchComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
